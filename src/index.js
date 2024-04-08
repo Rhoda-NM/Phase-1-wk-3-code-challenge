275bb5cad7f5692e0cbcc79eb99dc286f2600f17
RHODA NJERI MUYA <njerirhoda31@gmail.com>
2024-04-08 14:04:29 +0300

    code challenge

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 const filmList = document.getElementById('films');
-const filmItem = filmList.firstChild;
 const imgHolder = document.querySelector("img");
 const movieTitle =document.getElementById("title");
 const movieRuntime = document.getElementById("runtime");
@@ -9,74 +8,147 @@ const btn = document.getElementById("buy-ticket");
 const showTime = document.getElementById("showtime");
 const initialTickets = document.getElementById("ticket-num")
 
-//btn.addEventListener("click", getTickets)
-function getTickets(){
-
+movieTitle.addEventListener('click', hello);
+function hello() {
+    console.log("hello")
 }
 
-
-//console.log(imgHolder)
-
-function getFilms() {
-        return fetch(url)
-        .then((response) => response.json())
-        .then((data) => {
+document.addEventListener("DOMContentLoaded", () => {
+    fetch(url)
+    .then((response) => {
+        if (!response.ok) {
+            console.log("problem")
+            return 
+        }
+        //console.log(response.json)
+        return response.json()
+    })
+    .then((data) => {
             
-            console.log(data[0]);
-            const firstMovie = data[0];
-            const movieObj = data.map(movie => {
-                const li = document.createElement("list");
-                li.setAttribute("class", "film item")
-                li.innerHTML = movie.title;
-                filmList.appendChild(li);
-            })
-                showTime.innerHTML = firstMovie.showtime
-                movieTitle.innerHTML = firstMovie.title;
-                movieDescription.innerHTML = firstMovie.description;
-                movieRuntime.innerHTML = `${firstMovie.runtime} minute`;
-                imgHolder.src = firstMovie.poster;
-                initialTickets.innerHTML = firstMovie.capacity - firstMovie.tickets_sold;
-                
-                giantGilaMovie(data);
-                //console.log(movie)
-                
-            
-           
-        })
-            
-            
-           // movieObj(data);
-            
-            /*data.forEach(obj => {
-                Object.entries(obj).forEach(([key, value]) => {
-                    //console.log(`${key} ${value}`)
-                    if(key === "title") {
-                        const li = document.createElement("list");
-                        li.setAttribute("class", "film item")
-                        li.innerHTML = `${value}`;
-                         filmList.appendChild(li);
-                        //console.log(`${value}`)
-                    }
-                })
-            })*/
+        console.log(data);
         
-}
-getFilms()
-/*function (data) {
+        //Return a list of movie titles
+        renderMovies(data);
 
-}*/
-//function movieList(data
+        //render first movies information by default
+        const firstMovie = data[0];
+        moviePoster(firstMovie);   
     
-    
-
-function giantGilaMovie(){
-   
-    
-    /*const giantGila = data.find(movie => {
-        movie.id == 1
+    })
+    .catch((error) => {
+        console.log("Fetch error:" ,error);
+    })
+    function renderMovies(data){
+        const movieObj = data.map(movie => {
+            const container = document.createElement("div")
+            const li = document.createElement("list");
+            //Add a delete button next to the movie  name for deleting
+            const deleteBtn = document.createElement("button");
+            deleteBtn.innerHTML = "Delete";
+            container.appendChild(deleteBtn);
+            deleteBtn.addEventListener('click', (e) => {
+                //e.preventDefault();
+                removeMovie(movie);
+                console.log(movie.id)
+            })
+            //Create and populate li elements for movie titles
+            li.setAttribute("class", "film item")
+            li.innerHTML = movie.title;
+            li.appendChild(container);
+            //Add event listener to show movie poster for the clicked movie title instead of default first movie
+            li.addEventListener('click', () => {
+                let showingMovie = movie;
+                moviePoster(showingMovie);
+            })
+            filmList.appendChild(li); //Populate html element with the created lists
+        })
+    }
+     function removeMovie(movie){
         console.log(movie);
-    })*/
-    //movgiantGila.title
-    
-}
-//giantGilaMovie()
+        fetch(`url/${movie.id}`, {
+            method: "DELETE",
+        })
+        .then((res) => {
+            if(!res.ok){
+                throw new Error("Error occured");
+            }
+            return res.json();
+        })
+        .then((data) => {
+
+            const index = data.findIndex((m) => {
+                m.id === movie.id;
+                return m.id;
+            })
+            if(index !== 1){
+                data.splice(index, 1);
+            }
+        })
+        .catch((error) => {
+            console.log("Delete error:" ,error);
+        })
+     }
+
+     //Function for adding movie description
+     function moviePoster(currentMovie) {
+        showTime.innerHTML = `ShowTime: ${currentMovie.showtime}`
+        movieTitle.innerHTML = currentMovie.title;
+        movieDescription.innerHTML = currentMovie.description;
+        movieRuntime.innerHTML = `Runtime: ${currentMovie.runtime} minute`;
+        imgHolder.src = currentMovie.poster;
+        initialTickets.innerHTML = currentMovie.capacity - currentMovie.tickets_sold;
+
+        //toggle button attribute disabled true text content remove att
+        btn.setAttribute("disabled", "true");
+
+        //Update ticket button based ob tickets availabilty
+        if (currentMovie.tickets_sold >= currentMovie.capacity) {
+            btn.textContent = "Sold Out";
+            btn.setAttribute("disabled", "true");
+        } else {
+            btn.textContent = "Buy Ticket";
+            btn.removeAttribute("disabled");
+        }
+        btn.addEventListener("click", (e) => {
+            e.preventDefault();
+           // firstMovie.tickets_sold -=1;
+            //console.log(initialTickets);
+            buyTicket(currentMovie);
+            
+        });
+     }
+
+     function buyTicket(currentMovie) {
+        if (currentMovie) {
+            // Check if there are available tickets
+            if (currentMovie.tickets_sold < currentMovie.capacity) {
+                // Update the sold tickets count
+                currentMovie.tickets_sold++;
+                updateTickets(currentMovie);
+            }
+        }
+    }
+    function updateTickets(movie){
+
+        if(movie) {
+            fetch(`url/${movie.id}`, {
+                method: "PATCH",
+                headers: {
+                    'content-Type': 'application/JSON',
+                    'Accept': 'application/json',
+                },
+                body: JSON.stringify(movie)
+            })
+            .then((res) => res.json)
+            .then((updatedData) => {
+                movie = updatedData;
+            })
+            .catch((error) => {
+                console.log(`Patch Error: ${error}`);
+            })
+        }  
+    }
+
+})
+
+
